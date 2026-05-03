b53775208b6b7d4542ce97aff34355d1533d926a
Alejandro Jimenez <alejandro.jz1312@gmail.com>
2026-05-03 18:18:52 -0500

    chore: :zap: Better JS function management

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 alert("Portfolio under construction.");
+
+function toggleMenu() {
+    const item = document.getElementById('mobileMenu');
+    const item2 = document.getElementById('mobileIcon');
+    
+    item.classList.toggle('open');
+    item2.classList.toggle('active');
+
+    if (item2.classList.contains('active')) {
+        item2.innerHTML = "&#10006;";
+    } else {
+        item2.innerHTML = "&#9776;";
+    }
+}
+
+document.querySelector('.navbar-toggle').addEventListener('click', toggleMenu);

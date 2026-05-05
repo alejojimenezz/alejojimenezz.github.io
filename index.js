9906ae133f50c7136ad4197e3b5f8d5dbfbb09e9
Alejandro Jimenez <alejandro.jz1312@gmail.com>
2026-05-04 22:46:12 -0500

    test: :construction: Hover tabs test for portfolio

--- index.js
+++ index.js
@@ -15,3 +15,21 @@ function toggleMenu() {
 }
 
 document.querySelector('.navbar-toggle').addEventListener('click', toggleMenu);
+
+function openWork(evt, category) {
+
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].computedStyleMap.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(category).style.display = "block";
+    evt.currentTarget.className += " active";
+}
